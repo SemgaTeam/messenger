631712bee96b66b1557ec513afb797aaa12c9f61
losion <losion445@gmail.com>
2026-02-05 16:48:37 +0300

    [rework] useing hooks, better responcibility distribution.

---
+++ src/components/UserListItem.tsx
@@ -0,0 +1,19 @@
+import { User } from "@/hooks/UseUsers";
+
+export function UserListItem(
+    {
+        user,
+        onClick,
+    } : {
+        user: User,
+        onClick: () => void
+    }
+) {
+    return (
+        <button 
+            onClick={onClick} 
+            className="p-2 rounded cursor-pointer transition bg-blue-500 font-semibold hover:bg-gray-700">
+            {user.display_name}
+        </button>
+    );
+}
--- src/hooks/UseUsers.ts
+++ src/hooks/UseUsers.ts
@@ -1,15 +1,32 @@
-import { useState } from "react";
-
+import { useCallback, useState } from "react";
 export interface User {
     id: string;
     display_name: string;
     status: string;
 }
 
-export default function useUsers(_users: User[]) {
-    const [users, setUsers] = useState([]);
+export  function useUsers() {
+    const [users, setUsers] = useState<User[]>([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<string | null>(null);
 
-    const f = async function() {fetch("api/user").then(r => r.json()).then(setUsers)};
-    f();
-    return [users, setUsers]
-};
+    const loadUsers = useCallback(async () => {
+        if (users.length > 0) return; 
+
+        setLoading(true);
+        setError(null);
+
+        try {
+            const r = await fetch("/api/users");
+            if (!r.ok) throw new Error(`Error fetching users: ${r.statusText}`);
+            const data = await r.json();
+            setUsers(data);
+        } catch (err: any) {
+            setError("Failed to load users: " + err.message);
+        } finally {
+            setLoading(false)
+        }
+    }, [users]);
+
+    return  { users, loading, error, loadUsers };
+}
